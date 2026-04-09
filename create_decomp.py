a6b287aa4e038f0bdae0a6bec5d5a954d0986c66
Dustin Swales <dustin.swales@noaa.gov>
2026-04-09 15:21:56 +0000

    Update

--- create_decomp.py
+++ create_decomp.py
@@ -18,7 +18,7 @@ def main():
 #
     (mesh, decomps) = parse_args()
     for decomp in decomps:
-        com = "gpmetis -minconn -contig -niter=200 x1."+str(mesh)+".graph.info "+decomp
+        com = "gpmetis -minconn -contig -niter=200 x1."+str(mesh)+".graph.info "+str(decomp)
         print(com)
         result = os.system(com)
     # end for
